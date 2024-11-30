230ca8adf8fe6006f0f60a1bdec943e57b99e6b6
gennaro <002genny@gmail.com>
2024-11-30 10:52:35 +0100

    ciao

--- applicazione/back-end/index.js
+++ applicazione/back-end/index.js
@@ -1,3 +1,24 @@
 import express from "express";
+import morgan from 'morgan';
+import cors from "cors";
 
-console.log("ciao");
+//aggiungere import dei router
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan('dev'));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500).json({
+        code: err.status || 500,
+        description: err.message || "An error occurred"
+    });
+});
+
+//aggiungere l uso delle route
