7f74919fbfdc95f7d942832306ea910328137311
gennaro <002genny@gmail.com>
2024-12-27 12:20:46 +0100

    inizio fomr nuovo immobile

---
+++ applicazione/front-end/dietiEstates/src/app/nuovo-immobile/form-step1/form-step1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-step1',
+  imports: [],
+  templateUrl: './form-step1.component.html',
+  styleUrl: './form-step1.component.scss'
+})
+export class FormStep1Component {
+
+}
--- applicazione/front-end/dietiEstates/src/app/nuovo-immobile/nuovo-immobile.component.ts
+++ applicazione/front-end/dietiEstates/src/app/nuovo-immobile/nuovo-immobile.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {FormStep1Component} from './form-step1/form-step1.component';
 
 @Component({
   selector: 'app-nuovo-immobile',
-  imports: [],
+  imports: [FormStep1Component],
   templateUrl: './nuovo-immobile.component.html',
   styleUrl: './nuovo-immobile.component.scss'
 })
