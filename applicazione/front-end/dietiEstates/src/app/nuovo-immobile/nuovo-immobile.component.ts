01ffe657e3af8d2d3bf5e3a71904f0b123686ced
gennaro <002genny@gmail.com>
2024-12-28 11:19:16 +0100

    fine form inserimento immobile

--- applicazione/front-end/dietiEstates/src/app/nuovo-immobile/nuovo-immobile.component.ts
+++ applicazione/front-end/dietiEstates/src/app/nuovo-immobile/nuovo-immobile.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import {FormStep1Component} from './form-step1/form-step1.component';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuovo-immobile',
-  imports: [FormStep1Component],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './nuovo-immobile.component.html',
   styleUrl: './nuovo-immobile.component.scss'
 })
 export class NuovoImmobileComponent {
+  router = inject(Router);  //permette la navigazione
+  step: number = 1;
+  submittedStep1 = false;  //flag dello stato di invio del form
+    step1Form = new FormGroup({ //form per il login
+      tipo: new FormControl('', [Validators.required]), //campo di input dell'username
+      prezzo: new FormControl('', [Validators.required]),
+      descrizione: new FormControl('', [Validators.required]),
+      numeroStanze: new FormControl('', [Validators.required]),
+      dimensioni: new FormControl('', [Validators.required]),
+    })
 
+  submittedStep2 = false;  //flag dello stato di invio del form
+  step2Form = new FormGroup({ //form per il login
+    immagini: new FormControl('', [Validators.required]), //campo di input dell'username
+    altriServizi: new FormControl('', [Validators.required]),
+    classeEnergetica: new FormControl('', [Validators.required]),
+    piano: new FormControl('', [Validators.required]),
+  })
+
+  submittedStep3 = false;  //flag dello stato di invio del form
+  step3Form = new FormGroup({ //form per il login
+    citta: new FormControl('', [Validators.required]), //campo di input dell'username
+    comune: new FormControl('', [Validators.required]),
+    viaECivico: new FormControl('', [Validators.required]),
+  })
+
+  handleStep1Form(){
+    this.step = 2;
+  }
+
+  handleStep2Form(){
+    this.step = 3;
+  }
+
+  handleStep3Form(){
+    //this.step = 3;
+    this.router.navigateByUrl("/homePageAgenteImmobiliare");
+  }
+
+  backStep(){
+    this.step--;
+  }
 }
+
+
